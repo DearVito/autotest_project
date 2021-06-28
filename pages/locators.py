5123bc2a25d50f068ac755e179225d5664fdfefb
Dear Vito <mr.io-93@mail.ru>
2021-06-28 15:45:04 +0500

    product page tests added, code restructered

--- pages/locators.py
+++ pages/locators.py
@@ -1,15 +1,16 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, 'form#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, 'form#register_form') 
 
 class ProductPageLocators():
     BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
-    ADDED_PRODUCT = (By.XPATH, '//*[@id="messages"]/div[1]/div')
+    ADDED_PRODUCT = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
     BASKET_VALUE = (By.CSS_SELECTOR, 'div.alert p > strong')
     BOOK_PRICE = (By.CSS_SELECTOR,'p.price_color')
     PRODUCT_NAME = (By.CSS_SELECTOR, 'div.col-sm-6.product_main > h1')
+    SUCCESS_MESSAGE = (By.XPATH,'//*[@id="messages"]/div[1]/div')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
--- pages/product_page.py
+++ pages/product_page.py
@@ -9,13 +9,16 @@ class ProductPage(BasePage):
     def check_added_product_name(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         added_product = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT).text
-        assert product_name in added_product 
+        assert product_name == added_product
 
     def check_basket_value(self):
         book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
         basket_value = self.browser.find_element(*ProductPageLocators.BASKET_VALUE).text
         assert book_price == basket_value
 
-        # def should_be_login_link(self):
-        #     assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+        
+    def should_success_message_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should be disappeared"
 
